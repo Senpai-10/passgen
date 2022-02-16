a977afbc37dfb7d98104fba311a11f9e2fe755b8
senpai-10 <bmjfdrh@gmail.com>
2022-02-17 00:04:30 +0300

    c option

--- src/main.py
+++ src/main.py
@@ -1,6 +1,8 @@
-import click
+import random, string, click
 
 @click.command()
-@click.option("-c", default=16, help="Number of character in password")
+@click.option("-c", default=16, 
+              show_default=True, type=int, 
+              help="Number of character in password")
 def main(c):
-  print(c)
+  print("hi")
