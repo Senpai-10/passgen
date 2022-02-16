75a98db8d4d88b14c14c6fc1d02e8c1b5736dfb9
senpai-10 <bmjfdrh@gmail.com>
2022-02-17 00:57:03 +0300

    file and no-output options

--- src/main.py
+++ src/main.py
@@ -1,13 +1,15 @@
-import random, string, click
+import os, random, string, click
 
 @click.command()
+@click.option("-f", "--file", type=str, help="write passwords to file")
+@click.option("-no", "--no-output", type=bool, is_flag=True, help="no output")
 @click.option("-l", "--length", default=16, show_default=True, type=int, help="length of password")
 @click.option("-c", "--count", default=1, show_default=True, type=int, help="number of generated passwords")
 @click.option("-nl", "--no-lower", type=bool, help="no lower case character", is_flag=True)
 @click.option("-nu", "--no-upper", type=bool, help="no upper case character", is_flag=True)
 @click.option("-nn", "--no-num", type=bool, help="no numbers", is_flag=True)
 @click.option("-ns", "--no-symbols", type=bool, help="no symbols", is_flag=True)
-def main(count, length, no_lower, no_upper, no_num, no_symbols):
+def main(file, no_output, count, length, no_lower, no_upper, no_num, no_symbols):
   lower = string.ascii_lowercase
   upper = string.ascii_uppercase
   num = string.digits
@@ -25,7 +27,7 @@ def main(count, length, no_lower, no_upper, no_num, no_symbols):
   if int(count) > 1:
     for i in range(count):
       password = "".join(random.sample(chars, length))
-      click.echo(password)
+      if no_output == False: click.echo(password)
   elif int(count) == 1:
     password = "".join(random.sample(chars, length))
-    click.echo(password)
+    if no_output == False: click.echo(password)
